f8a1e71eb09da18ea0b369099f6bf3a514dad293
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-11-11 14:43:29 -0600

    Ejercicio: implementando un CPU

---
+++ cpu.py
@@ -0,0 +1,44 @@
+#hola
+
+('ADD', 'Ra', 'Rb', 'Rc')
+('SUB', 'Ra', 'Rb', 'Rc')
+('MUL', 'Ra', 'Rb', 'Rc')
+('DIV', 'Ra', 'Rb', 'Rc')
+('INC', 'Ra')
+('DEC', 'Ra')
+('CMP', 'op', 'Ra', 'Rb', 'Rc') # op = {<, >, <=, >=, ==, !=}
+('CONST', value, 'Ra')
+('LOAD', 'Rs', 'Rd', offset) # Simulando nuestra RAM
+('STORE', 'Rs', 'Rd', offset) # Simulando nuestra RAM
+('JMP', 'Rd', offset) # Mueve el IP a la dirección de memoria
+('HALT')
+
+
+class CPU:
+    # implementa un CPU muy simpre que ejecute las instrucciones de arriba
+    # 1. Leer el programa
+    # 2. Ejecutar el programa
+
+    def run(self, program):
+        self.registers = {'Ra': 0, 'Rb': 0, 'Rc': 0, 'Rd': 0} # 8 registros
+        self.memory = [0] * 1024
+        self.registers['IP'] = 0
+        self.registers['SP'] = 0
+        # Leer las instruvvioes hasta el final e imprimir el resultado
+
+    def ADD(self, arg1, arg2, result):  # 'Ra', 'Rb', 'Rc'
+        self.registers[result] = self.registers[arg1] + self.registers[arg2]
+
+    def JMP(self, arg1, offset):
+        self.registers['IP'] = self.registers[arg1] + offset
+
+
+machine = CPU()
+code = [
+    ('CONST', 10, 'Ra'),
+    ('CONST', 20, 'Rb'),
+    ('ADD', 'Ra', 'Rb', 'Rc'),
+    ('HALT')
+]
+
+machine.run(code)
