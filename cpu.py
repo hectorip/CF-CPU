61071b678bfe6573dbf1e11281f3157f01f4dc58
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-11-13 23:46:04 -0600

    Aclaración del ejercicio

--- cpu.py
+++ cpu.py
@@ -1,17 +1,17 @@
-# hola
+# Completa el siguiente código para simular un CPU que trabaje con registros
 
-('ADD', 'Ra', 'Rb', 'Rc')
-('SUB', 'Ra', 'Rb', 'Rc')
-('MUL', 'Ra', 'Rb', 'Rc')
-('DIV', 'Ra', 'Rb', 'Rc')
-('INC', 'Ra')
-('DEC', 'Ra')
-('CMP', 'op', 'Ra', 'Rb', 'Rc')  # op = {<, >, <=, >=, ==, !=}
-('CONST', value, 'Ra')
-('LOAD', 'Rs', 'Rd', offset)  # Simulando nuestra RAM
-('STORE', 'Rs', 'Rd', offset)  # Simulando nuestra RAM
-('JMP', 'Rd', offset)  # Mueve el IP a la dirección de memoria
-('HALT')
+# ('ADD', 'Ra', 'Rb', 'Rc')
+# ('SUB', 'Ra', 'Rb', 'Rc')
+# ('MUL', 'Ra', 'Rb', 'Rc')
+# ('DIV', 'Ra', 'Rb', 'Rc')
+# ('INC', 'Ra')
+# ('DEC', 'Ra')
+# ('CMP', 'op', 'Ra', 'Rb', 'Rc')  # op = {<, >, <=, >=, ==, !=}
+# ('CONST', value, 'Ra')
+# ('LOAD', 'Rs', 'Rd', offset)  # Simulando nuestra RAM
+# ('STORE', 'Rs', 'Rd', offset)  # Simulando nuestra RAM
+# ('JMP', 'Rd', offset)  # Mueve el IP a la dirección de memoria
+# ('HALT')
 
 
 class CPU:
@@ -20,25 +20,20 @@ class CPU:
     # 2. Ejecutar el programa
 
     def run(self, program):
-        self.registers = {'Ra': 0, 'Rb': 0, 'Rc': 0, 'Rd': 0}  # 8 registros
+        self.registers = {"Ra": 0, "Rb": 0, "Rc": 0, "Rd": 0}  # 8 registros
         self.memory = [0] * 1024
-        self.registers['IP'] = 0
-        self.registers['SP'] = 0
+        self.registers["IP"] = 0
+        self.registers["SP"] = 0
         # Leer las instruccioes hasta el final e imprimir el resultado
 
     def ADD(self, arg1, arg2, result):  # 'Ra', 'Rb', 'Rc'
         self.registers[result] = self.registers[arg1] + self.registers[arg2]
 
     def JMP(self, arg1, offset):
-        self.registers['IP'] = self.registers[arg1] + offset
+        self.registers["IP"] = self.registers[arg1] + offset
 
 
 machine = CPU()
-code = [
-    ('CONST', 10, 'Ra'),
-    ('CONST', 20, 'Rb'),
-    ('ADD', 'Ra', 'Rb', 'Rc'),
-    ('HALT')
-]
+code = [("CONST", 10, "Ra"), ("CONST", 20, "Rb"), ("ADD", "Ra", "Rb", "Rc"), ("HALT")]
 
 machine.run(code)
